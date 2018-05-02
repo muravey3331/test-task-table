91866ef1f2603aa2882416c868de9746921e6053
Pave Bevz <pavlik143@gmail.com>
2018-05-02 20:21:52 +0300

    commit before pull

--- src/index.js
+++ src/index.js
@@ -5,12 +5,12 @@ import './index.css';
 import Table from './components/Table';
 
 const initialState = {
-    headers: ['name', 'last name', 'age'],
+    headers: ['name', 'last name', 'age', 'city'],
     data: [
-        ['Thomas', 'Cruse', 25],
-        ['Jack', 'Nick', 17],
-        ['Anton', 'Shevchuk', 30],
-        ['Max', 'Durov', 20]
+        ['Thomas', 'Cruse', 25, 'London'],
+        ['Jack', 'Nick', 17, 'New York'],
+        ['Anton', 'Shevchuk', 30, 'Moscow'],
+        ['Max', 'Durov', 20, 'Kyiv']
     ]
 };
 
